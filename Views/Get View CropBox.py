04343369f8477a44e0796d2426907079325bf535
gokermu <51519350+gokermu@users.noreply.github.com>
2021-09-03 12:29:13 +0300

    Create Get View CropBox.py

---
+++ Views/Get View CropBox.py
@@ -0,0 +1,42 @@
+import clr
+clr.AddReference('System.Core')
+clr.AddReference('RhinoInside.Revit')
+clr.AddReference('RevitAPI') 
+clr.AddReference('RevitAPIUI')
+
+from System import Enum
+
+import rhinoscriptsyntax as rs
+import Rhino
+import RhinoInside
+import Grasshopper
+from Grasshopper.Kernel import GH_RuntimeMessageLevel as RML
+from RhinoInside.Revit import Revit, Convert
+from Autodesk.Revit import DB
+
+# access the active document object
+doc = Revit.ActiveDBDocument
+
+def show_warning(msg):
+    ghenv.Component.AddRuntimeMessage(RML.Warning, msg)
+
+def show_error(msg):
+    ghenv.Component.AddRuntimeMessage(RML.Error, msg)
+
+def show_remark(msg):
+    ghenv.Component.AddRuntimeMessage(RML.Remark, msg)
+
+View = View if isinstance(View, list) else [View]
+
+CropBoxCurve = []
+Active = []
+Visible = []
+
+for v in View:
+    Active.append(v.CropBoxActive)
+    Visible.append(v.CropBoxVisible)
+    crop = v.GetCropRegionShapeManager().GetCropShape()
+    if len(crop)>0:
+        CropBoxCurve.append(crop)
+    else:
+        CropBoxCurve.append(None)

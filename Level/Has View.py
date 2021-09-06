26389a812811e82828eef3e77f7f745c81322885
gokermu <51519350+gokermu@users.noreply.github.com>
2021-09-06 12:57:07 +0300

    Create Has View.py

---
+++ Level/Has View.py
@@ -0,0 +1,40 @@
+#This definition checks whether if the given level has an associated plan view or not.
+#Gives a boolean in return.
+
+import clr
+clr.AddReference('System.Core')
+clr.AddReference('RhinoInside.Revit')
+clr.AddReference('RevitAPI') 
+clr.AddReference('RevitAPIUI')
+
+from System import Enum
+
+import rhinoscriptsyntax as rs
+import Rhino
+import RhinoInside
+import Grasshopper
+from Grasshopper.Kernel import GH_RuntimeMessageLevel as RML
+from RhinoInside.Revit import Revit, Convert
+from Autodesk.Revit import DB
+
+# access the active document object
+doc = Revit.ActiveDBDocument
+
+def show_warning(msg):
+    ghenv.Component.AddRuntimeMessage(RML.Warning, msg)
+
+def show_error(msg):
+    ghenv.Component.AddRuntimeMessage(RML.Error, msg)
+
+def show_remark(msg):
+    ghenv.Component.AddRuntimeMessage(RML.Remark, msg)
+
+Bool = []
+
+Level = Level if isinstance(Level, list) else [Level]
+
+for l in Level:
+    if l.FindAssociatedPlanViewId():
+        Bool.append(True)
+    else:
+        Bool.append(False)

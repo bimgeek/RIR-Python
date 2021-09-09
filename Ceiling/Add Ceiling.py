765ab4c363201e0f190dc86ead1c4349383a17b7
gokermu <51519350+gokermu@users.noreply.github.com>
2021-09-09 17:54:42 +0300

    Update Add Ceiling.py

--- Ceiling/Add Ceiling.py
+++ Ceiling/Add Ceiling.py
@@ -1,10 +1,13 @@
+__author__ = "mbgoker"
+__version__ = "2021.09.09"
+
 import clr
 clr.AddReference('System.Core')
 clr.AddReference('RhinoInside.Revit')
 clr.AddReference('RevitAPI') 
 clr.AddReference('RevitAPIUI')
 
-from System import Enum
+from System import Enum, Action
 
 import rhinoscriptsyntax as rs
 import Rhino
@@ -12,14 +15,15 @@ import RhinoInside
 import Grasshopper
 from Grasshopper.Kernel import GH_RuntimeMessageLevel as RML
 from RhinoInside.Revit import Revit, Convert
+# add extensions methods as well
+# this allows calling .ToXXX() convertor methods on Revit objects
+clr.ImportExtensions(Convert.Geometry)
 from Autodesk.Revit import DB
-
-clr.ImportExtensions(RhinoInside.Revit.Convert.Geometry)
-from System.Collections.Generic import List
 
 # access the active document object
 doc = Revit.ActiveDBDocument
 
+# a few utility methods
 def show_warning(msg):
     ghenv.Component.AddRuntimeMessage(RML.Warning, msg)
 
@@ -29,45 +33,45 @@ def show_error(msg):
 def show_remark(msg):
     ghenv.Component.AddRuntimeMessage(RML.Remark, msg)
 
-#Output list of ceilings
+#import treehelp library when working with DataTrees.
+import ghpythonlib.treehelpers as th
+
+#This is for converting single items to list so we can loop through it.
+#IN = IN if isinstance(IN, list) else [list]
+
+# write your code here
+"""
+with DB.Transaction(doc, "Give it a descriptive name") as t:
+    t.Start()
+    try:
+        
+        t.Commit()
+    except Exception as txn_err:
+        show_error(str(txn_err))
+        t.RollBack()
+"""
+
+#Output list of ceilings.
 Ceilings = []
 
-#Converting data tree to a list of lists-
-BoundaryList = []
-
-#Counting how many branch does the data tree have.
-BranchCount = Boundary.BranchCount
-
-#Looping data tree and appending elements to the newly created list.
-for i in range(BranchCount):
-    BoundaryList.append(Boundary.Branch(i))
-
-#Making sure that Types is a list input containing the same amount of elements as BoundaryList.
+#Ceiling Types as a List
 Types = []
 
+#Room Boundary count (just in case when we want to apply a single type to all rooms.)
+BoundaryBranchCount = Boundary.BranchCount
+
+#Converting data trees to nested lists.
+Boundary = th.tree_to_list(Boundary)
+Type = th.tree_to_list(Type)
+
+#Making sure length of Type and Boundary inputs are same.
 if len(Type) == 1:
-    for i in range(len(BoundaryList)):
-        Types.append(Type[0])
-
-elif Boundary.BranchCount == 0:
-    show_remark("This component expects a boundary.")
-
-elif len(Type) != len(BoundaryList):
+    for i in range(BoundaryBranchCount):
+        Types.append(Type)
+elif BoundaryBranchCount == 0:
+    show_warning("This component expects a boundary.")
+elif len(Type) != len(Boundary):
     show_warning("Boundary and Type inputs do not contain same amount of elements.")
-
 else:
     Types = Type
 
-with DB.Transaction(doc, "Create Ceilings") as t:
-    t.Start()
-        #First, loop through each branch and create a curve loop. This curveloop will be the boundary of our floor.
-        #This curveloop will create the ceiling with opening.
-    for b, ty in zip(BoundaryList, Types):
-        CurveLoop = List[DB.CurveLoop]()
-        #Then Loop through each polyline/curve and append the to the CurveLoop list.
-        #Do not forget to convert them to Revit equivalents.
-        for c in b:
-            CurveLoop.Add(c.ToCurveLoop())
-        ceiling = DB.Ceiling.Create(doc, CurveLoop, ty.Id, Level.Id)
-        Ceilings.append(ceiling)
-    t.Commit()

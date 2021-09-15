998a83d39be9e565d6be0cbdfca720c0f805e311
gokermu <51519350+gokermu@users.noreply.github.com>
2021-09-15 09:27:36 +0300

    Update Room by Point.py

--- Room/Room by Point.py
+++ Room/Room by Point.py
@@ -1,20 +1,30 @@
+__author__ = "mbgoker"
+__version__ = "2021.09.09"
+
 import clr
+clr.AddReference('System.Core')
 clr.AddReference('RhinoInside.Revit')
 clr.AddReference('RevitAPI') 
+clr.AddReference('RevitAPIUI')
 
-from System import Enum
+from System import Enum, Action
 
+import rhinoscriptsyntax as rs
+import Rhino
 import RhinoInside
+import Grasshopper
 from Grasshopper.Kernel import GH_RuntimeMessageLevel as RML
-from RhinoInside.Revit import Revit
+from RhinoInside.Revit import Revit, Convert
+# add extensions methods as well
+# this allows calling .ToXXX() convertor methods on Revit objects
+clr.ImportExtensions(Convert.Geometry)
 from Autodesk.Revit import DB
-
-clr.ImportExtensions(RhinoInside.Revit.Convert.Geometry)
 from Rhino.Geometry import Point2d
 
 # access the active document object
 doc = Revit.ActiveDBDocument
 
+# a few utility methods
 def show_warning(msg):
     ghenv.Component.AddRuntimeMessage(RML.Warning, msg)
 
@@ -24,17 +34,16 @@ def show_error(msg):
 def show_remark(msg):
     ghenv.Component.AddRuntimeMessage(RML.Remark, msg)
 
-Rooms = []
+#This is for converting single items to list so we can loop through it.
+Point = Point if isinstance(Point, list) else [Point]
+Room = []
 
-if len(Points) == 1:
-    Points = [Points]
-
-with DB.Transaction(doc, "Create Rooms by Points") as t:
+with DB.Transaction(doc, "Add Rooms by Point and Level") as t:
     t.Start()
     try:
-        for point in Points:
+        for point in Point:
             room = doc.Create.NewRoom(Level, Point2d(point).ToUV())
-            Rooms.append(room)
+            Room.append(room)
         t.Commit()
     except Exception as txn_err:
         show_error(str(txn_err))
